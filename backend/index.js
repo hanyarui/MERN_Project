1aab4726e9f75af623c8d8942810a697c4d4ce6f
Dharu Aulia Rahman <106579507+ruinzyu@users.noreply.github.com>
2023-07-24 17:02:21 +0700

    Update index

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import userRoute from "./routes/UserRoute.js";
 const app = express();
 app.use(cors(
   {
-    origin: ["https://mern-project-api.vercel.app"],
+    origin: [""],
     methods: ["POST", "GET"],
     credentials: true
   }
